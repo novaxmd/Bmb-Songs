d9beb43638efa0d2657c2c8e0df180248b08d4f9
Bmb Tech <hanchobmb@gmail.com>
2026-03-07 23:22:54 +0300

    Create utils.ts

---
+++ src/utils/utils.ts
@@ -0,0 +1,57 @@
+export const genres = [
+  "indie",
+  "edm",
+  "metal",
+  "punk",
+  "party",
+  "jazz",
+  "love",
+  "rap",
+  "workout",
+  "pop",
+  "hiphop",
+  "rock",
+  "melody",
+  "lofi",
+  "chill",
+  "focus",
+  "instrumental",
+  "folk",
+  "devotional",
+  "ambient",
+  "sleep",
+  "soul",
+];
+
+export const defaultSearchData = {
+  topQuery: {
+    results: [],
+    position: 0,
+  },
+  songs: {
+    results: [],
+    position: 0,
+  },
+  albums: {
+    results: [],
+    position: 0,
+  },
+  artists: {
+    results: [],
+    position: 0,
+  },
+  playlists: {
+    results: [],
+    position: 0,
+  },
+};
+
+export const PLAYER_CONSTANTS = {
+  MOBILE_BREAKPOINT: 640,
+  DEFAULT_VOLUME: 0.5,
+  VOLUME_THRESHOLDS: {
+    MUTE: 0.1,
+    MEDIUM: 0.75,
+  },
+};
+  

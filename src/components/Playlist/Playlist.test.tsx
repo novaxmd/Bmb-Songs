34abdc92926544805512944a9d59140edc14db2e
Bmb Tech <hanchobmb@gmail.com>
2026-03-07 22:52:15 +0300

    Add files via upload

---
+++ src/components/Playlist/Playlist.test.tsx
@@ -0,0 +1,99 @@
+import {
+  act,
+  cleanup,
+  fireEvent,
+  render,
+  renderHook,
+  screen,
+  waitFor,
+} from "@testing-library/react";
+import { afterAll, afterEach, describe, expect, test, vi } from "vitest";
+import Playlist from "./Playlist";
+import fallback from "/fallbacks/playlist-fallback.webp";
+import { samplePlaylistOfList } from "../../api/samples";
+import { MemoryRouter, useLocation } from "react-router-dom";
+
+const fadeOutNavigate = vi.fn();
+fadeOutNavigate.mockImplementation(() => {});
+
+afterEach(() => {
+  cleanup();
+  vi.useRealTimers();
+});
+
+afterAll(() => {
+  vi.clearAllMocks();
+  vi.clearAllTimers();
+});
+
+describe("Playlist", () => {
+  test("should render", () => {
+    render(
+      <Playlist {...samplePlaylistOfList} fadeOutNavigate={fadeOutNavigate} />,
+    );
+    expect(screen.getByTestId("playlist")).toBeInTheDocument();
+  });
+  test("should fadeout and navigate on click", () => {
+    vi.useFakeTimers();
+    render(
+      <Playlist {...samplePlaylistOfList} fadeOutNavigate={fadeOutNavigate} />,
+    );
+    const { result } = renderHook(() => useLocation(), {
+      wrapper: MemoryRouter,
+    });
+    const path = result.current.pathname;
+    act(() => {
+      fireEvent.click(screen.getByTestId("playlist"));
+    });
+    vi.advanceTimersByTime(150);
+    waitFor(() => {
+      expect(path).toBe(`/playlists/${samplePlaylistOfList.id}`);
+    });
+  });
+  test("should contain image of size 150x150 if found", () => {
+    render(
+      <Playlist
+        {...{
+          ...samplePlaylistOfList,
+          image: [
+            {
+              quality: "150x150",
+              url: "image url",
+            },
+          ],
+        }}
+        fadeOutNavigate={fadeOutNavigate}
+      />,
+    );
+    expect(screen.getByAltText("Playlist 21")).toHaveAttribute(
+      "src",
+      "image url",
+    );
+  });
+  test("should contain fallback image if no image", () => {
+    render(
+      <Playlist {...samplePlaylistOfList} fadeOutNavigate={fadeOutNavigate} />,
+    );
+    expect(screen.getByAltText("Playlist 21")).toHaveAttribute("src", fallback);
+  });
+  test("image should be it's fallback onError", () => {
+    render(
+      <Playlist {...samplePlaylistOfList} fadeOutNavigate={fadeOutNavigate} />,
+    );
+    fireEvent.error(screen.getByAltText("Playlist 21"));
+    expect(screen.getByAltText("Playlist 21")).toHaveAttribute("src", fallback);
+  });
+  test("image should have transition applied on mount", () => {
+    vi.useFakeTimers();
+    render(
+      <Playlist {...samplePlaylistOfList} fadeOutNavigate={fadeOutNavigate} />,
+    );
+    expect(screen.getByAltText("Playlist 21")).toBeInTheDocument();
+    expect(screen.getByAltText("Playlist 21")).toHaveClass("image-fadeout");
+    expect(screen.getByAltText("Playlist 21")).not.toHaveClass("image-fadein");
+    vi.advanceTimersByTime(samplePlaylistOfList.i * 50);
+
+    expect(screen.getByAltText("Playlist 21")).toHaveClass("image-fadein");
+    expect(screen.getByAltText("Playlist 21")).not.toHaveClass("image-fadeout");
+  });
+});
---
+++ src/components/Playlist/Playlist.tsx
@@ -0,0 +1,57 @@
+import { PlaylistOfList } from "../../types/GlobalTypes";
+import fallback from "/fallbacks/playlist-fallback.webp";
+import { useEffect, useRef } from "react";
+
+export default function Playlist({
+  id,
+  image,
+  name,
+  i,
+  fadeOutNavigate,
+}: PlaylistOfList & { i: number; fadeOutNavigate: (str: string) => void }) {
+  const playlistImgEl = useRef<HTMLImageElement>(null);
+
+  const getPlaylistImage = () => {
+    if (image) {
+      const obj = image.find((img) => img.quality === "150x150");
+      if (obj) return obj.url;
+      else return fallback;
+    }
+    return fallback;
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      playlistImgEl.current?.classList.remove("image-fadeout");
+      playlistImgEl.current?.classList.add("image-fadein");
+    }, i * 50);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [i]);
+
+  return (
+    <button
+      data-testid="playlist"
+      className="group mr-4 flex h-[180px] w-[150px] flex-shrink-0 cursor-pointer list-none flex-col items-center overflow-hidden outline-none"
+      onClick={() => fadeOutNavigate(`/playlists/${id}`)}
+    >
+      <div className="h-[150px] w-[150px] overflow-hidden">
+        <img
+          ref={playlistImgEl}
+          src={getPlaylistImage()}
+          alt={name}
+          loading="eager"
+          fetchPriority="high"
+          width={150}
+          height={150}
+          className="image-fadeout scale-105 shadow-md shadow-black brightness-100 transition-all ease-linear group-hover:scale-100 group-hover:brightness-90 group-focus:scale-100 group-focus:brightness-90"
+          onError={(e) => (e.currentTarget.src = fallback)}
+        />
+      </div>
+      <p className="mt-2 line-clamp-1 text-ellipsis whitespace-pre-line text-center text-xs text-neutral-400 transition-colors ease-linear group-hover:text-white group-focus:text-white">
+        {name}
+      </p>
+    </button>
+  );
+}
